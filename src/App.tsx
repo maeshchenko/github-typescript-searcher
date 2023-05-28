eba85b546899465f25626cf01f30af04132ee7e7
Mikhail Eshchenko <mikhail.eshchenko@gmail.com>
2023-05-28 19:29:44 +0700

    done first mvp

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState, useCallback } from 'react';
+import { Octokit } from 'octokit';
 
 function App() {
+  interface IItems {
+    id: number,
+    stars: number,
+    name: string,
+    url: string
+  }
+
+  const octokit = new Octokit({ auth: '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' });
+
+  const [items, setItems] = useState<IItems[]>([]);
+  const [page, setPage] = useState(0);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const getData = async (page: number) => {
+    setIsLoading(true);
+    const response = await octokit.rest.search.repos({
+      q: 'language:TypeScript',
+      sort: "stars",
+      per_page: 20,
+      page: page,
+    })
+      .then(response => {
+        const formattedItems = response.data?.items.map(({ id, stargazers_count, html_url, full_name }) => ({ id, name: full_name, stars: stargazers_count, url: html_url }));
+
+        setItems(formattedItems);
+        setIsLoading(false);
+      })
+  };
+
+  useEffect(() => {
+    getData(page);
+  }, [page])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {!isLoading && <>
+        <h1>Page: {page}</h1>
+        {items && items.map(item => <li key={item.id}><a href={item.url}>{item.name}</a></li>)}
+        <button onClick={() => {
+          if (page > 0) { setPage(page - 1) }
+        }}>←</button>
+        <button onClick={() => setPage(page + 1)}>→</button>
+      </>}
+      {isLoading && <p>Loading...</p>}
+    </>
   );
 }
 
